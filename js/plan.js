11f3fc33ec3d24842b374f717cce3db67fe73a6b
Garik1502 <30408381+Garik1502@users.noreply.github.com>
2020-01-30 16:58:48 +0200

    Update plan.js

--- js/plan.js
+++ js/plan.js
@@ -4,7 +4,7 @@ var arr_plan = [
 	`<a href="https://garik1502.github.io/Blockgs/plan/a&b/hy.html" class="note_a">
 		<div id="note_div" style="background-image: url(https://github.com/Garik1502/Blockgs/blob/master/plan/a&b/A&B.png?raw=true);">
 		<h3 class="note_name">
-				Интерактивный армянский алфавит
+				Армянский алфавит
 		</h3>
 	 </div></a>`
 ];
