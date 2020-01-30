1be0f4e72f1964d2687a82d253357b1f8af0c776
Garik1502 <30408381+Garik1502@users.noreply.github.com>
2020-01-30 16:44:50 +0200

    Update plan.js

--- js/plan.js
+++ js/plan.js
@@ -1,18 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 var arr_plan = [
-	`<div id="note_div" style="background-image: url(https://html5book.ru/wp-content/uploads/2015/10/html5.png);">
+	`<div id="note_div" style="background-image: url(https://github.com/Garik1502/Blockgs/blob/master/plan/a&b/A&B.png);">
 		<h3 class="note_name">
-			<a href="lorem_ipsum.html" class="note_a">
-				Text: 1Lorem ipsum parabelle
-			</a>
-		</h3>
-	 </div>`,
-
-	`<div id="note_div" style="background-image: url(https://html5book.ru/wp-content/uploads/2015/10/html5.png);">
-		<h3 class="note_name">
-			<a href="lorem_ipsum.html" class="note_a">
-				Text: 2Lorem ipsum parabelle
+			<a href="https://garik1502.github.io/Blockgs/plan/a&b/hy.html" class="note_a">
+				Интерактивный армянский алфавит
 			</a>
 		</h3>
 	 </div>`
