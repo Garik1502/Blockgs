da65f183b799314d733876c617f2a76e03137ec6
Garik1502 <30408381+Garik1502@users.noreply.github.com>
2020-01-31 19:47:18 +0200

    Update news.js

--- js/news.js
+++ js/news.js
@@ -12,7 +12,7 @@ window.onload = function() {
 var arr = [
 	`<div id="news"><h3>
 <a name="#6" href="news.html#6">#6</a> 
-На Відміну Від - Я боюся Чебурашку<iframe width="560" height="315" src="https://www.youtube.com/embed/2n0PJqHVGcQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+На Відміну Від - Я боюся Чебурашку<br><iframe width="560" height="315" src="https://www.youtube.com/embed/2n0PJqHVGcQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </h3></div>`,
 	
 	`<div id="news"><h3>
