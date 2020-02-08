749ea11b5739a98df40c319aab587f56cd12441f
Garik1502 <30408381+Garik1502@users.noreply.github.com>
2020-02-08 13:41:03 +0200

    Update note.js

--- js/note.js
+++ js/note.js
@@ -1,53 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+/*	
+`<div id="note_div" style="background-image: url(https://html5book.ru/wp-content/uploads/2015/10/html5.png);">
+<h3 class="note_name">
+<a href="lorem_ipsum.html" class="note_a">
+Text: Lorem ipsum parabelle
+</a></h3></div>`,	
+*/
+
 var arr_note = [
-	`<div id="note_div" style="background-image: url(https://html5book.ru/wp-content/uploads/2015/10/html5.png);">
-		<h3 class="note_name">
-			<a href="lorem_ipsum.html" class="note_a">
-				Text: 1Lorem ipsum parabelle
-			</a>
-		</h3>
-	 </div>`,
-
-	`<div id="note_div" style="background-image: url(https://html5book.ru/wp-content/uploads/2015/10/html5.png);">
-		<h3 class="note_name">
-			<a href="lorem_ipsum.html" class="note_a">
-				Text: 2Lorem ipsum parabelle
-			</a>
-		</h3>
-	 </div>`,
-
-	`<div id="note_div" style="background-image: url(https://html5book.ru/wp-content/uploads/2015/10/html5.png);">
-		<h3 class="note_name">
-			<a href="lorem_ipsum.html" class="note_a">
-				Text: 3Lorem ipsum parabelle
-			</a>
-		</h3>
-	 </div>`,
-
-	`<div id="note_div" style="background-image: url(https://html5book.ru/wp-content/uploads/2015/10/html5.png);">
-		<h3 class="note_name">
-			<a href="lorem_ipsum.html" class="note_a">
-				Text: 1Lorem ipsum parabelle
-			</a>
-		</h3>
-	 </div>`,
-
-	`<div id="note_div" style="background-image: url(https://html5book.ru/wp-content/uploads/2015/10/html5.png);">
-		<h3 class="note_name">
-			<a href="lorem_ipsum.html" class="note_a">
-				Text: 2Lorem ipsum parabelle
-			</a>
-		</h3>
-	 </div>`,
-
-	`<div id="note_div" style="background-image: url(https://html5book.ru/wp-content/uploads/2015/10/html5.png);">
-		<h3 class="note_name">
-			<a href="lorem_ipsum.html" class="note_a">
-				Text: 3Lorem ipsum parabelle
-			</a>
-		</h3>
-	 </div>`
+	
+`<div id="note_div" style="background-image: url(https://sun9-67.userapi.com/c855120/v855120386/1f3a3d/sCqdOdRZzfw.jpg);">
+<h3 class="note_name">
+<a href="note/1.html" class="note_a">
+Антисоветские пасхалки в армянских мультфильмах
+</a></h3></div>`
+	
 ];
 
 
