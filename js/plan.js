4c67a1fa32643a640e4afa92e48fa3c4f43d6194
Garik1502 <30408381+Garik1502@users.noreply.github.com>
2020-03-27 17:14:33 +0200

    Update plan.js

--- js/plan.js
+++ js/plan.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 var arr_plan = [
-	`<a href="https://garik1502.github.io/Blockgs/plan/a&b/hy.html" class="note_a">
-		<div id="note_div" style="background-image: url(https://github.com/Garik1502/Blockgs/blob/master/plan/a&b/A&B.png?raw=true);">
-		<h3 class="note_name">
-				Армянский алфавит
-		</h3>
-	 </div></a>`
+	
+`<a href="https://garik1502.github.io/Blockgs/plan/a&b/hy.html" class="note_a">
+<div id="note_div" style="background-image: url(https://github.com/Garik1502/Blockgs/blob/master/plan/a&b/A&B.png?raw=true);">
+<h3 class="note_name">Армянский алфавит</h3></div></a>`
+	
 ];
 
 
