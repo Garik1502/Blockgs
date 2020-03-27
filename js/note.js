268502862c39637076c94c9772a43963a23ff6cb
Garik1502 <30408381+Garik1502@users.noreply.github.com>
2020-03-27 17:12:01 +0200

    Update note.js

--- js/note.js
+++ js/note.js
@@ -1,20 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 /*	
-`<a href="note/1.html" class="note_a">
-<div id="note_div" style="background-image: url(https://sun9-67.userapi.com/c855120/v855120386/1f3a3d/sCqdOdRZzfw.jpg);">
-<h3 class="note_name">
-Антисоветские пасхалки в армянских мультфильмах
-</h3></div></a>`,	
+`<a href="note/НОМЕР.html" class="note_a">
+<div id="note_div" style="background-image: url(ФОТО);">
+<h3 class="note_name">Название</h3></div></a>`,	
 */
 
 var arr_note = [
 	
 `<a href="note/1.html" class="note_a">
 <div id="note_div" style="background-image: url(https://sun9-67.userapi.com/c855120/v855120386/1f3a3d/sCqdOdRZzfw.jpg);">
-<h3 class="note_name">
-Антисоветские пасхалки в армянских мультфильмах
-</h3></div></a>`	
+<h3 class="note_name">Антисоветские пасхалки в армянских мультфильмах</h3></div></a>`	
 	
 ];
 
