3b7f9787f296f2223f7b053a205e31137703466e
Garik1502 <30408381+Garik1502@users.noreply.github.com>
2020-07-01 21:05:09 +0300

    Update plan.js

--- js/plan.js
+++ js/plan.js
@@ -4,7 +4,11 @@ var arr_plan = [
 	
 `<a href="https://garik1502.github.io/Blockgs/plan/a&b/hy.html" class="note_a">
 <div id="note_div" style="background-image: url(https://github.com/Garik1502/Blockgs/blob/master/plan/a&b/A&B.png?raw=true);">
-<h3 class="note_name">Армянский алфавит</h3></div></a>`
+<h3 class="note_name">Армянский алфавит</h3></div></a>`,
+
+`<a href="https://garik1502.github.io/Blockgs/plan/book/one.pdf" class="note_a">
+<div id="note_div" style="background-image: url(https://www.litmir.me/data/Book/0/659000/659819/BC3_1572188466.jpg?raw=true);">
+<h3 class="note_name">АМО САГИЯН — «Перед закатом»</h3></div></a>`
 	
 ];
 
