7135063b17a4034ae84e3caac8c1c422e83f96a9
Garik1502 <30408381+Garik1502@users.noreply.github.com>
2020-07-29 11:56:17 +0300

    Update news.js

--- js/news.js
+++ js/news.js
@@ -12,6 +12,11 @@ window.onload = function() {
 var arr = [
 	
 `<div id="news"><h3>
+<a name="#2" href="news.html#2">#2</a>
+<img src="https://scontent-waw1-1.xx.fbcdn.net/v/t1.0-9/116261583_129952265447657_1521549068888838068_o.jpg?_nc_cat=100&_nc_sid=730e14&_nc_ohc=gkOU0BrDK6EAX9YWuou&_nc_ht=scontent-waw1-1.xx&oh=b92c1b1f899e6bbc52109de4e783d636&oe=5F4513E4" width="300" tabindex="0"/>
+</h3></div>`,	
+	
+`<div id="news"><h3>
 <a name="#1" href="news.html#1">#1</a><br>
 —Вечером свободны вы?<br>
 —Нет<br>
